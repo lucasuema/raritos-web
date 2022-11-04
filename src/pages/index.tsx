e35988cfd71c531d66c797b2830b31e6cc0d2e69
lucasuema <47237357+lucasuema@users.noreply.github.com>
2022-11-04 20:08:58 -0300

    improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ const IndexPage = () => {
   const router = useRouter();
 
   return (
-    <Layout title="Ecoterreans">
+    <Layout title="The Raritos">
       <div className="flex flex-col h-screen bg-black ">
         <div className=" flex flex-col flex-1 mt-[50%] sm:mt-[15%] justify-center items-center text-center overflow-clip">
           <h1 className="text-5xl text-white font-bold ">The raritos 👹</h1>
-          <div className="px-10 pt-10 md:max-w-lg">Experimenting with the web and metaverse.</div>
+          <div className="px-10 pt-10 text-white md:max-w-lg">7471 PFPs coming soon...👀</div>
           <div className="flex w-64 justify-center gap-5 pt-10">
             {/* <button
               className=" bg-green-400 p-2  w-32 rounded-md shadow-[2px_2px_0_rgba(0,0,0,1)] hover:shadow-none "
